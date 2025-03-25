8dabf5e0b496a7b0980059083e2d1ec479a4ded5
Vinaya <vinayarajput0716@gmail.com>
2025-03-25 12:09:52 +0530

    falling stars

--- scripts.js
+++ scripts.js
@@ -20,3 +20,24 @@ birthdayCard.addEventListener("mouseleave", function () {
     mySound.pause();
     mySound.currentTime = 0; // Reset to the beginning
 });
+
+const container = document.querySelector('body');
+const snowflake = document.querySelector('.snowflake');
+
+function appendSnowflake() {
+    const newSnowflake = snowflake.cloneNode(true);
+
+    // Each snowflake looks and behaves a little different:
+    newSnowflake.style.paddingLeft = Math.random() * 10 + 'px';
+    newSnowflake.style.animationDuration =
+        Math.random() * 5 + 3 + 's';
+    newSnowflake.style.opacity = Math.random() * 1;
+
+    container.append(newSnowflake);
+}
+
+const interval = setInterval(appendSnowflake, 50);
+
+setTimeout(() => {
+    clearInterval(interval);
+}, 2000);
